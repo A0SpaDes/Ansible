abb389c187e4618ac55ef0060904335555a37def
A0SpaDes <127371916+A0SpaDes@users.noreply.github.com>
2024-03-10 15:14:32 +0700

    Update zabbix-agent-DR-Ubu18.sh

--- zabbix-agent-DR-Ubu18.sh
+++ zabbix-agent-DR-Ubu18.sh
@@ -4,7 +4,7 @@
 wget 10.96.1.212/ubuntu/download/zabbix-agent/ubuntu18/zabbix-agent2-dbgsym_6.4.12-1+ubuntu18.04_amd64.deb
 
 # Download dczabbix_agentd.conf
-wget 10.96.1.212/ubuntu/download/zabbix-agent/ubuntu18/dczabbix_agentd.conf
+wget 10.96.1.212/ubuntu/download/zabbix-agent/ubuntu18/drzabbix_agentd.conf
 
 # Install libpcre2-8-0 package
 apt install libpcre2-8-0 -y
@@ -13,7 +13,7 @@ apt install libpcre2-8-0 -y
 dpkg -i zabbix-agent2-dbgsym_6.4.12-1+ubuntu18.04_amd64.deb
 
 # Move dczabbix_agentd.conf to /etc/zabbix
-mv dczabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf -f
+mv drzabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf -f
 
 # Replace placeholder in zabbix_agentd.conf
 sed -i "s/thaythe/$HOSTNAME /g" /etc/zabbix/zabbix_agentd.conf
