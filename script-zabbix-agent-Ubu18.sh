162a7fab4b02bf31d1d9a8dcf2aef9f862464cdf
A0SpaDes <127371916+A0SpaDes@users.noreply.github.com>
2024-03-10 15:12:33 +0700

    Create script-zabbix-agent-Ubu18.sh

---
+++ script-zabbix-agent-Ubu18.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Download zabbix-agent2-dbgsym package
+wget 10.96.1.212/ubuntu/download/zabbix-agent/ubuntu18/zabbix-agent2-dbgsym_6.4.12-1+ubuntu18.04_amd64.deb
+
+# Download dczabbix_agentd.conf
+wget 10.96.1.212/ubuntu/download/zabbix-agent/ubuntu18/dczabbix_agentd.conf
+
+# Install libpcre2-8-0 package
+apt install libpcre2-8-0 -y
+
+# Install zabbix-agent2-dbgsym package
+dpkg -i zabbix-agent2-dbgsym_6.4.12-1+ubuntu18.04_amd64.deb
+
+# Move dczabbix_agentd.conf to /etc/zabbix
+mv dczabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf -f
+
+# Replace placeholder in zabbix_agentd.conf
+sed -i "s/thaythe/$HOSTNAME /g" /etc/zabbix/zabbix_agentd.conf
+
+# Restart zabbix-agent service
+systemctl restart zabbix-agent.service
+
+# Enable zabbix-agent service
+systemctl enable zabbix-agent.service
