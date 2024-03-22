11891aa274883def66c6d09b47013de6e3c4eaf0
A0SpaDes <127371916+A0SpaDes@users.noreply.github.com>
2024-03-22 13:20:03 +0700

    Create create-service-account.sh

---
+++ create-service-account.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+read -p 'Enter the name of ServiceAccount : ' name
+
+export CLIENT=$name
+echo -e "\nUsername is: ${CLIENT}
+
+#Generate ServiceAccount
+
+cat << EOF >> ${CLIENT}-sa.yaml
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: ${CLIENT}-sa
+  namespace: kubernetes-dashboard
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: ${CLIENT}-sa
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: ${CLIENT}-sa
+  namespace: kubernetes-dashboard
+EOF
+
+kubectl apply -f ${CLIENT}-sa.yaml
+kubectl -n kubernetes-dashboard create token ${CLIENT}-sa
