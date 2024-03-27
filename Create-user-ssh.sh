d5cd7b8c6bc7e2dc3c9977cb32778b81bdabe3b1
A0SpaDes <127371916+A0SpaDes@users.noreply.github.com>
2024-03-27 09:57:45 +0700

    Update Create-user-ssh.sh

--- Create-user-ssh.sh
+++ Create-user-ssh.sh
@@ -6,10 +6,10 @@ OQsvcj175woZ3LwlPFpR8ChiQNNUJFW9b4udpZ37wD2nxQ4fZHK9zWqAPFo5i8ag
 bVpuy376deR5zUFrTUV594XZgQnD1Q9gy6A0TSLQRwHDqPbVwOTRkTxnErzrRhAl
 KGZJojJn0tgz8RxmCvLhy/1vGMF91n9RkZwEQtv9Bcl2gYkx+RNK7hWWsiFuzGpk
 Z7iN3gx6WQsYPsJr7FYQ02a0aXR+52gzW7d+nnL3EZH09oY2eQ== haubc"
-/usr/sbin/useradd $user
-/bin/mkdir -p /home/$user/.ssh
-grep -rl "$sshkey" /home/$user/.ssh/authorized_keys
-if [ $? != 0 ]; then echo "$sshkey" >> /home/$user/.ssh/authorized_keys; fi
-grep -rl "$user" /etc/sudoers
-if [ $? != 0 ]; then echo "$user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers; fi
-chown -R $user.$user /home/$user
+/usr/sbin/useradd ${user}
+/bin/mkdir -p /home/${user}/.ssh
+grep -rl "${sshkey}" /home/${user}/.ssh/authorized_keys
+if [ $? != 0 ]; then echo "${sshkey}" >> /home/${user}/.ssh/authorized_keys; fi
+grep -rl "${user}" /etc/sudoers
+if [ $? != 0 ]; then echo "${user} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers; fi
+chown -R ${user}.${user} /home/${user}
