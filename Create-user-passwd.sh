e34fea15bb5438eba00155becc5d8d742866d43d
A0SpaDes <127371916+A0SpaDes@users.noreply.github.com>
2024-05-13 09:02:26 +0700

    Update Create-user-passwd.sh

--- Create-user-passwd.sh
+++ Create-user-passwd.sh
@@ -9,4 +9,8 @@ adduser --quiet --disabled-password --shell /bin/bash --home /home/$user --gecos
 echo "$user:$password" | chpasswd
 echo User: $user has been created with password: $password
 
+# approve sudo
+# grep -rl "${user}" /etc/sudoers
+# if [ $? != 0 ]; then echo "${user} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers; fi
+
 #SCRIPT END
